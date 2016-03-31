53ae350f177efa5bebb9b14ab800395884f1006f
rampartisan <alexbaldwinaudio@gmail.com>
2016-03-31 17:28:18 +0200

    BasicSpatilaiserTemplateWithDebug
    Template spatialiser native audio plugin for unity, includes interop
    functions that allow the plugin (C++) to send debugs to Unity console.
    Mac build only.

---
+++ Mac OS/SpatialiserTemplate/EditorCode/cppDebug.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class cppDebug : MonoBehaviour {
+
+	private delegate void DebugCallback(string message);
+
+	[DllImport("AudioPluginSpatializerTemplate")]
+	private static extern void RegisterDebugCallback(DebugCallback callback);
+
+	private void Start()
+	{
+		RegisterDebugCallback(new DebugCallback(DebugMethod));
+	}
+
+	private static void DebugMethod(string message)
+	{
+		Debug.Log("NAPlugin: " + message);
+	}
+}
